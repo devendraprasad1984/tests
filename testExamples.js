de32e8a0aff373a58b0605c34d4d60236212a763
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-03 15:43:24 +0530

    updates

--- testExamples.js
+++ testExamples.js
@@ -327,6 +327,7 @@ const testClassEs6Types = () => {
         }
     }
 
+
     let dpr = new test('dpresume')
     let devn = new test('devendra')
     for (let u of [dpr, devn]) {
@@ -1199,4 +1200,55 @@ const minWindowSubstring = () => {
     }
     console.log(checkLowerSubstringExistance(["ahffaksfajeeubsne", "jefaa"]))
 }
-minWindowSubstring()
+// minWindowSubstring()
+
+
+const arrayNestingMain = () => {
+    //leetcode problem
+    // You are given an integer array nums of length n where nums is a permutation of the numbers in the range [0, n - 1].
+    //
+    //     You should build a set s[k] = {nums[k], nums[nums[k]], nums[nums[nums[k]]], ... } subjected to the following rule:
+    //
+    //     The first element in s[k] starts with the selection of the element nums[k] of index = k.
+    //     The next element in s[k] should be nums[nums[k]], and then nums[nums[nums[k]]], and so on.
+    //     We stop adding right before a duplicate element occurs in s[k].
+    //     Return the longest length of a set s[k].
+    //
+    // Input: nums = [5,4,0,3,1,6,2]
+    // Output: 4
+    // Explanation:
+    //     nums[0] = 5, nums[1] = 4, nums[2] = 0, nums[3] = 3, nums[4] = 1, nums[5] = 6, nums[6] = 2.
+    // One of the longest sets s[k]:
+    // s[0] = {nums[0], nums[5], nums[6], nums[2]} = {5, 6, 2, 0}
+    //Input: nums = [0,1,2]
+    // Output: 1
+
+    var arrayNesting = function (nums) {
+        if (nums.length === 0) return []
+        if (nums.length === 1) return nums
+
+        let isDuplicateFound = false
+        let obj = {}
+        let maxLen = 0
+        for (let i of nums) {
+            let res = [i]
+            let curIterIndex = i
+            while (!isDuplicateFound) {
+                let valueAtIndex = nums[curIterIndex]
+                isDuplicateFound = res.indexOf(valueAtIndex) !== -1
+                if (!isDuplicateFound) {
+                    res.push(valueAtIndex)
+                    curIterIndex = valueAtIndex
+                }
+            }
+            obj[i] = res
+            maxLen = res.length > maxLen ? res.length : maxLen
+            // console.log('running for ', i, res)
+        }
+        return {maxLen, obj}
+    };
+    // console.log(arrayNesting([5, 4, 0, 3, 1, 6, 2])) //4
+    console.log(arrayNesting([0, 2, 1])) //2
+}
+
+arrayNestingMain()
