3cf48cf94d25258508b20cf62efc096a20d58cb3
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-14 14:18:13 +0530

    updates

--- dynamicChessboard/js/index.js
+++ dynamicChessboard/js/index.js
@@ -1,19 +1,18 @@
-const myChessBoardDom = document.getElementById("chessboard")
-
-const drawChessBoard = (rows = 8, columns = 8) => {
+const drawChessBoard = (elem, rows = 8, columns = 8) => {
+    const myChessBoardDom = document.getElementById(elem)
     let board = []
     for (let row = 0; row < rows; row++) {
         let line = []
         for (let col = 0; col < columns; col++) {
-            line.push(`<span class="cell center" id="cell_${col}">(${row+1} - ${col + 1})</span>`)
+            let odd_even = (row + col) % 2
+            let cellColor = odd_even === 0 ? 'black' : 'white'
+            line.push(`<span class="cell center ${cellColor}" id="cell_${col}">(${row + 1} - ${col + 1})</span>`)
         }
-        board.push(`<div class="line" id="row_${row+1}">${line.join('')}</div>`)
+        board.push(`<div class="line" id="row_${row + 1}">${line.join('')}</div>`)
     }
-    myChessBoardDom.innerHTML=board.join('')
+    myChessBoardDom.innerHTML = board.join('')
 }
 
-(function (){
-    drawChessBoard()
-    // drawChessBoard(9,9)
-    // drawChessBoard(10,10)
+(function () {
+    drawChessBoard('chessboard', 8, 8)
 })()
