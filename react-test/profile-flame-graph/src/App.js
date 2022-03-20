dc8470e9b18783a41d38112828594cd90ab9529c
devendra prasad <devendraprasad1984@gmail.com>
2022-03-20 12:34:03 +0530

    update

--- react-test/profile-flame-graph/src/App.js
+++ react-test/profile-flame-graph/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FlameChart from "./FlameChart";
+import AutoSizer from "react-virtualized-auto-sizer";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null
+  };
+  loadData = () => {
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(data => this.setState({ data }));
+  };
+  render() {
+    const { data } = this.state;
+    if (data === null) {
+      return <button onClick={this.loadData}>Load data</button>;
+    }
+    return (
+        <div className="App">
+          <AutoSizer>
+            {({ height, width }) => (
+                <FlameChart data={data} height={height} width={width} />
+            )}
+          </AutoSizer>
+        </div>
+    );
+  }
 }
 
 export default App;
