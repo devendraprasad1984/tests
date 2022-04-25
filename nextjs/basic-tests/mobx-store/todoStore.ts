ab680a9dba8b8a49bb26425b45d8aa6da38ecc74
devendra prasad <devendraprasad1984@gmail.com>
2022-04-25 08:32:19 +0530

    mobx next

--- nextjs/basic-tests/mobx-store/todoStore.ts
+++ nextjs/basic-tests/mobx-store/todoStore.ts
@@ -1,4 +1,4 @@
-import {configure, makeAutoObservable, observable} from "mobx";
+import {action, computed, configure, makeAutoObservable, observable} from "mobx";
 
 export interface ITodoItem {
     id: number,
@@ -17,10 +17,25 @@ class TodoStore {
     constructor() {
         makeAutoObservable(this, {
             todoList: observable,
-            todo: observable
+            todo: observable,
+            addTodo: action,
+            toggleTodo: action,
+            status: computed
         })
         // 2 args: 1 - this context, all props point to observer
         //observes changes on this class context and rerenders the UI when changes happen from anywhere in app
+    }
+
+    //computed - gives about metadata from store rather changing store
+    get status() {
+        let completed = 0, remaining = 0
+        this.todoList.forEach(todo => {
+            if (todo.done)
+                completed++
+            else
+                remaining++
+        })
+        return {completed, remaining}
     }
 
     initTodo() {
@@ -37,7 +52,15 @@ class TodoStore {
         this.todoList.push(this.todo)
         this.todo = this.initTodo()
     }
+
+    toggleTodo(id: number) {
+        console.log(`inside toggle to ${id}`)
+        let index: number = this.todoList.findIndex((t: ITodoItem) => t.id === id)
+        if (index !== -1) {
+            this.todoList[index].done = !this.todoList[index].done
+        }
+    }
 }
 
-const todoStore = new TodoStore()
+const todoStore = new TodoStore() //store should be singleton
 export default todoStore
